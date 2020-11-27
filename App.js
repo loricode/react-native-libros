a84a7cc2aa0fd8d62cda1aa676a30d20bcf4da92
loricode <loricode2793@gmail.com>
2020-11-27 18:41:31 -0500

    primer commit

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, FlatList ,Text, View } from 'react-native';
+import axios from 'axios'
+
+const URL="http://192.168.1.1/applibrolaravel/public/libro" //o localhost
+
+//components
+import Card from './components/Card'
 
 export default function App() {
+
+  const [ listaLibros, setListaLibro ] = useState([])
+
+  useEffect(() => {
+    getlibros()
+  },[])
+
+  const getlibros = async() => {
+    const { data }  = await axios.get(URL)
+    const { libros } = data
+    setListaLibro(libros)
+    console.log(data)
+  }
+  
+  const renderItem = ( { item }) => (
+     <Card nombre={item.nombre} edicion={item.edicion} />
+   ) 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 0.2, backgroundColor: 'steelblue'}} >
+        <Text style={styles.title}>Libros</Text>
+      </View>
+      
+      <FlatList
+        data={listaLibros}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()} 
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +46,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',   
   },
+  title:{
+    marginTop:15,
+    marginLeft:15,
+    fontSize:18,
+    color:'#fff'
+  }
 });
---
+++ components/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+ function Card({ nombre ,edicion }) {
+  
+    return (
+      <View style={styles.cardView}>
+        <Text>Nombre: {nombre}</Text>
+        <Text>Edicion: {edicion}</Text>
+      </View>
+    );
+  }
+
+
+  
+const styles = StyleSheet.create({
+    cardView: {
+        backgroundColor: "white",
+        borderRadius: 20,
+        marginVertical:5,
+        marginHorizontal:9,
+        alignSelf:'stretch',
+        padding: 35,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 3,
+        alignSelf:'stretch',
+
+      },
+  });
+  export default Card;
